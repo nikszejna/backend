e6724b8c90925ad3937a8139ea264a86f6669d40
Nik Szejna <szejna@gmail.com>
2021-12-02 20:39:10 -0700

    Add doc string for assumption

--- dashboard/query_utils.py
+++ dashboard/query_utils.py
@@ -1,6 +1,8 @@
 import re
 
+
 def extract_videoid(url):
+    """Returns youtube video id from url, assumes is a url or standalone video id"""
     try:
         id = re.search('^https://.*watch?\?v=(.*)$', url)
         return id.group(1)
